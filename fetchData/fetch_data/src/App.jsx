0a7a88d3dcb824f1884ecadbdae3f30060c176fc
Chandrikavishwas <chandrikavishwas@gmail.com>
2025-06-23 19:51:40 +0530

    fetchData

--- fetchData/fetch_data/src/App.jsx
+++ fetchData/fetch_data/src/App.jsx
@@ -1,30 +1,25 @@
-import { useState ,useEffect } from 'react'
-import axios from 'axios';
-
+import { useState, useEffect } from "react";
 import './App.css'
 
-function App() {
-  const [data, setData] = useState([])
-  const [toggle, setToggle] = useState(false)
+function App(){
+
+      const [data, setData] = useState([])
 
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res =>{
-      setData(res.data)
-    })
-    .catch(err=>console.log(err))
+    fetch(`https://jsonplaceholder.typicode.com/albums`)
+    .then((res)=>res.json())
+    .then((data)=>setData(data))
+    .catch((err)=>console.log(err))
   })
-    return (
-    <>
-      {!toggle && <button onClick={()=>setToggle(true)}>Get Data</button>}
-      { toggle &&
-        data.map(res =>{
-          return <div key={res.id}>{res.id}  {res.title}</div>
-        })
-      }
-          
-    </>
-  )
+  return(<>
+        {data.map((el)=>{
+          return(<div key={el.id}>
+                  {el.id} {el.title}
+          </div>)
+        })}
+  </>)
 }
 
-export default App
+
+export default App;
+
